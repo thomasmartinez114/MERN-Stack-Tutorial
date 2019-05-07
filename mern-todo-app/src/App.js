8b0564b90e9dc236328ff4c5ce6b2f577656d70d
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-05-07 04:42:42 +0000

    push

---
+++ mern-todo-app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+class App extends Component {
+  render() {
+    return (
+      
+        <div className="container">
+          <h2>MERN-Stack Todo App</h2>
+        </div>
+      
+    );
+  }
+}
+
+export default App;
